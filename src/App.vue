4139afc1f831b65ab944e5cab42daa4c9ff5ce53
mikhail.nikolaenko@itgarden.ru <sl;dfkhHKLJH23416>
2023-01-09 21:56:16 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <GameField/>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameField from './components/GameField.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {GameField}
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/components/GameField.vue
@@ -0,0 +1,425 @@
+<template>
+    <div class="wrapper">
+        <div class="top-row">
+            <div class="box box-big"></div>
+            <div class="box">
+                <div class="top-street-col red"></div>
+                <p class="top-street-name">Strand</p>
+                <p class="price price-top"> <span class="price-label">M</span> 220</p>
+            </div>
+            <div class="box">
+                <font-awesome-icon icon="fa-solid fa-question"/>
+            </div>
+            <div class="box">
+                <div class="top-street-col red"></div>
+                <p class="top-street-name">Fleet Street</p>
+                <p class="price price-top"> <span class="price-label">M</span> 220</p>
+            </div>
+            <div class="box">
+                <div class="top-street-col red"></div>
+                <p class="top-street-name">Trafalgar Square</p>
+                <p class="price price-top"> <span class="price-label">M</span> 240</p>
+            </div>
+            <div class="box">
+                <p class="station-name station-name-top">Marylebone <br> Station</p>
+                <img class="station station-top" src="../assets/img/train.png" alt="">
+                <p class="price price-top"> <span class="price-label">M</span> 200</p>
+            </div>
+            <div class="box">
+                <div class="top-street-col yellow"></div>
+                <p class="top-street-name">Liecister Square</p>
+                <p class="price price-top"> <span class="price-label">M</span> 260</p>
+            </div>
+            <div class="box">
+                <div class="top-street-col yellow"></div>
+                <p class="top-street-name">Coventry Street</p>
+                <p class="price price-top"> <span class="price-label">M</span> 260</p>
+            </div>
+            <div class="box"></div>
+            <div class="box">
+                <div class="top-street-col yellow"></div>
+                <p class="top-street-name">Piccadilly</p>
+                <p class="price price-top"> <span class="price-label">M</span> 280</p>
+            </div>
+            <div class="box box-big"></div>
+        </div>
+        <div class="side-rows">
+            <div class="left-row">
+                <div class="box">
+                    <div class="left-street-col orange"></div>
+                    <p class="left-street-name">Vine <br> Street</p>
+                    <p class="price price-left"> <span class="price-label">M</span> 200</p>
+                </div>
+                <div class="box">
+                    <div class="left-street-col orange"></div>
+                    <p class="left-street-name">Marlborough <br> Street</p>
+                    <p class="price price-left"> <span class="price-label">M</span> 180</p>
+                </div>
+                <div class="box"></div>
+                <div class="box">
+                    <div class="left-street-col orange"></div>
+                    <p class="left-street-name">Bow <br> Street</p>
+                    <p class="price price-left"> <span class="price-label">M</span> 180</p>
+                </div>
+                <div class="box">
+                    <p class="station-name station-name-left">Marylebone <br> Station</p>
+                    <img class="station station-left" src="../assets/img/train.png" alt="">
+                    <p class="price price-left"> <span class="price-label">M</span> 200</p>
+                </div>
+                <div class="box">
+                    <div class="left-street-col pink"></div>
+                    <p class="left-street-name">Northumrl <br> Avenue</p>
+                    <p class="price price-left"> <span class="price-label">M</span> 160</p>
+                </div>
+                <div class="box">
+                    <div class="left-street-col pink"></div>
+                    <p class="left-street-name">Whitehall</p>
+                    <p class="price price-left"> <span class="price-label">M</span> 140</p>
+                </div>
+                <div class="box"></div>
+                <div class="box">
+                    <div class="left-street-col pink"></div>
+                    <p class="left-street-name">Pall Mall</p>
+                    <p class="price price-left"> <span class="price-label">M</span> 140</p>
+                </div>
+            </div>
+            <div class="right-row">
+                <div class="box">
+                    <div class="right-street-col green"></div>
+                    <p class="right-street-name">Regent <br> Street</p>
+                    <p class="price price-right"> <span class="price-label">M</span> 300</p>
+                </div>
+                <div class="box">
+                    <div class="right-street-col green"></div>
+                    <p class="right-street-name">Oxford <br> Street</p>
+                    <p class="price price-right"> <span class="price-label">M</span> 300</p>
+                </div>
+                <div class="box"></div>
+                <div class="box">
+                    <div class="right-street-col green"></div>
+                    <p class="right-street-name">Bond <br> Street</p>
+                    <p class="price price-right"> <span class="price-label">M</span> 320</p>
+                </div>
+                <div class="box">
+                    <p class="station-name station-name-right">Live Rpool St. <br> Station</p>
+                    <img class="station station-right" src="../assets/img/train.png" alt="">
+                    <p class="price price-right"> <span class="price-label">M</span> 200</p>
+                </div>
+                <div class="box"></div>
+                <div class="box">
+                    <div class="right-street-col blue"></div>
+                    <p class="right-street-name">Park <br> Lane</p>
+                    <p class="price price-right"> <span class="price-label">M</span> 350</p>
+                </div>
+                <div class="box"></div>
+                <div class="box">
+                    <div class="right-street-col blue"></div>
+                    <p class="right-street-name">MayFair</p>
+                    <p class="price price-right"> <span class="price-label">M</span> 400</p>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-row">
+            <div class="box box-big"></div>
+            <div class="box">
+                <div class="bottom-street-col light-blue"></div>
+                <p class="bottom-street-name">Pentonville Road</p>
+                <p class="price price-bottom"> <span class="price-label">M</span> 120</p>
+            </div>
+            <div class="box">
+                <div class="bottom-street-col light-blue"></div>
+                <p class="bottom-street-name">Euston Road</p>
+                <p class="price price-bottom"> <span class="price-label">M</span> 100</p>
+            </div>
+            <div class="box"></div>
+            <div class="box">
+                <div class="bottom-street-col light-blue"></div>
+                <p class="bottom-street-name">The Angel, Islington</p>
+                <p class="price price-bottom"> <span class="price-label">M</span> 100</p>
+            </div>
+            <div class="box">
+                <p class="station-name station-name-bottom">Kings Cross Station</p>
+                <img class="station" src="../assets/img/train.png" alt="">
+                <p class="price price-bottom"> <span class="price-label">M</span> 200</p>
+            </div>
+            <div class="box"></div>
+            <div class="box">
+                <div class="bottom-street-col brown"></div>
+                <p class="bottom-street-name">White Chapel Road</p>
+                <p class="price price-bottom"> <span class="price-label">M</span> 60</p>
+            </div>
+            <div class="box"></div>
+            <div class="box">
+                <div class="bottom-street-col brown"></div>
+                <p class="bottom-street-name">Old Kent Road</p>
+                <p class="price price-bottom"> <span class="price-label">M</span> 60</p>
+            </div>
+            <div class="box box-big"></div>
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    padding: 0;
+}
+
+.wrapper {
+    margin: 10px;
+
+    width: 707px;
+    height: 707px;
+
+    display: flex;
+    flex-direction: column;
+
+    border: 1px solid black;
+
+}
+
+.box {
+    width: 20px;
+    height: 20px;
+
+    
+
+    border: 1px solid grey;
+}
+
+
+
+/* Верхний и нижний ряды */
+
+.top-row, .bottom-row {
+    display: flex;
+}
+
+.top-row .box, .bottom-row .box {
+    width: 59px;
+    height: 77px;
+
+    position: relative;
+
+    padding: 0;
+    margin: 0;
+}
+
+.top-row .box-big, .bottom-row .box-big {
+    width: 77px;
+    height: 77px;
+}
+
+
+
+/* Левый и правый ряды */
+
+.side-rows {
+    display: flex;
+    justify-content: space-between;
+}
+
+.left-row .box, .right-row .box{
+    width: 77px;
+    height: 59px;
+
+    position: relative;
+}
+
+/* Улицы */
+
+.top-street-col {
+    width: 59px;
+    height: 20px;
+
+    position: absolute;
+    bottom: 0;
+}
+
+.left-street-col {
+    width: 20px;
+    height: 59px;
+
+    position: absolute;
+    right: 0;
+}
+
+.right-street-col {
+    width: 20px;
+    height: 59px;
+
+    position: absolute;
+    left: 0;
+}
+
+.bottom-street-col {
+    width: 59px;
+    height: 20px;
+
+    position: absolute;
+    top: 0;
+}
+
+.orange {
+    background-color: orange;
+}
+
+.pink {
+    background-color: rgb(223, 15, 102);
+}
+
+.red {
+    background-color: red;
+}
+
+.yellow {
+    background-color: yellow;
+}
+
+.green {
+    background-color: green;
+}
+
+.blue {
+    background-color: blue;
+}
+
+.light-blue {
+    background-color: rgb(140, 212, 234);
+}
+
+.brown {
+    background-color: brown;
+}
+
+.top-street-name {
+    text-align: center;
+    font-size: 10px;
+    transform: rotate(180deg);
+    margin-top: 28px;
+}
+
+.right-street-name {
+    text-align: center;
+    font-size: 10px;
+    transform: rotate(270deg);
+    margin-top: 18px;
+}
+
+.left-street-name {
+    text-align: center;
+    font-size: 10px;
+    transform: rotate(90deg);
+    margin-top: 20px;
+}
+
+.bottom-street-name {
+    text-align: center;
+    font-size: 10px;
+    margin-top: 25px;
+}
+
+/* Цены  */
+
+.price-label {
+    text-decoration: line-through;
+}
+
+.price {
+    font-size: 12px;
+    position: absolute;
+}
+
+.price-top {
+    transform: rotate(180deg);
+    top: -12px;
+    left: 12px;
+}
+
+.price-left {
+    transform: rotate(90deg);
+    left: -10px;
+    top: 10px
+}
+
+.price-right {
+    transform: rotate(270deg);
+    top: 12px;
+    right: -10px;
+}
+
+.price-bottom {
+    bottom: -12px;
+    left: 14px;
+}
+
+/* Станции */
+
+.station {
+    display: block;
+    width: 37px;
+    height: 27px;
+    margin: 0 auto;
+}
+
+.station-top {
+    transform: rotate(180deg);
+    position: absolute;
+    left: 10px;
+    top: 22px;
+}
+
+.station-left {
+    transform: rotate(90deg);
+    position: absolute;
+    right: 22px;
+    top: 16px;
+}
+
+.station-right {
+    transform: rotate(270deg);
+    position: absolute;
+    left: 22px;
+    top: 15px;
+}
+
+.station-name {
+    font-size: 10px;
+    text-align: center;
+}
+
+.station-name-top {
+    transform: rotate(180deg);
+    position: absolute;
+    bottom: -7px;
+    left: 4px;
+}
+
+.station-name-left {
+    transform: rotate(90deg);
+    position: absolute;
+    right: -10px;
+    top: 8px;
+}
+
+.station-name-right {
+    transform: rotate(270deg);
+    position: absolute;
+    left: -15px;
+    top: 6px;
+}
+
+.station-name-bottom {
+    margin-top: 3px;
+    margin-bottom: 1px;
+}
+
+</style>
