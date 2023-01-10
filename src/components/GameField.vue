bacd420ec2d522f69174f4d2faa49fbea0e89dab
mikhail.nikolaenko@itgarden.ru <sl;dfkhHKLJH23416>
2023-01-10 23:01:17 +0300

    main gamefields

--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -8,7 +8,8 @@
                 <p class="price price-top"> <span class="price-label">M</span> 220</p>
             </div>
             <div class="box">
-                <font-awesome-icon icon="fa-solid fa-question"/>
+                <p class="chance-text chance-text-top">Chance</p>
+                <font-awesome-icon icon="fa-solid fa-question" class="question question-top"/>
             </div>
             <div class="box">
                 <div class="top-street-col red"></div>
@@ -35,7 +36,11 @@
                 <p class="top-street-name">Coventry Street</p>
                 <p class="price price-top"> <span class="price-label">M</span> 260</p>
             </div>
-            <div class="box"></div>
+            <div class="box">
+                <p class="water-works-text">Water Works</p>
+                <font-awesome-icon icon="fa-solid fa-faucet-drip" class="water-works"/>
+                <p class="price price-top"> <span class="price-label">M</span> 150</p>
+            </div>
             <div class="box">
                 <div class="top-street-col yellow"></div>
                 <p class="top-street-name">Piccadilly</p>
@@ -55,7 +60,10 @@
                     <p class="left-street-name">Marlborough <br> Street</p>
                     <p class="price price-left"> <span class="price-label">M</span> 180</p>
                 </div>
-                <div class="box"></div>
+                <div class="box">
+                    <p class="chest-text chest-text-left">Community <br> Chest</p>
+                    <img src="../assets/img/chest.png" alt="" class="chest chest-left">
+                </div>
                 <div class="box">
                     <div class="left-street-col orange"></div>
                     <p class="left-street-name">Bow <br> Street</p>
@@ -76,7 +84,11 @@
                     <p class="left-street-name">Whitehall</p>
                     <p class="price price-left"> <span class="price-label">M</span> 140</p>
                 </div>
-                <div class="box"></div>
+                <div class="box">
+                    <p class="electricity-text">Electric <br> Community</p>
+                    <img src="../assets/img/Lamp.png" alt="" class="electricity">
+                    <p class="price price-left"> <span class="price-label">M</span> 150</p>
+                </div>
                 <div class="box">
                     <div class="left-street-col pink"></div>
                     <p class="left-street-name">Pall Mall</p>
@@ -94,7 +106,10 @@
                     <p class="right-street-name">Oxford <br> Street</p>
                     <p class="price price-right"> <span class="price-label">M</span> 300</p>
                 </div>
-                <div class="box"></div>
+                <div class="box">
+                    <p class="chest-text chest-text-right">Community <br> Chest</p>
+                    <img src="../assets/img/chest.png" alt="" class="chest chest-right">
+                </div>
                 <div class="box">
                     <div class="right-street-col green"></div>
                     <p class="right-street-name">Bond <br> Street</p>
@@ -105,13 +120,20 @@
                     <img class="station station-right" src="../assets/img/train.png" alt="">
                     <p class="price price-right"> <span class="price-label">M</span> 200</p>
                 </div>
-                <div class="box"></div>
+                <div class="box">
+                    <p class="chance-text chance-text-right">Chance</p>
+                    <font-awesome-icon icon="fa-solid fa-question" class=" question question-right"/>
+                </div>
                 <div class="box">
                     <div class="right-street-col blue"></div>
                     <p class="right-street-name">Park <br> Lane</p>
                     <p class="price price-right"> <span class="price-label">M</span> 350</p>
                 </div>
-                <div class="box"></div>
+                <div class="box">
+                    <p class="supertax-text">Super Tax</p>
+                    <img src="../assets/img/ring.png" alt="" class="supertax">
+                    <p class="price supertax-price"> pay <span class="price-label">M</span> 100</p>
+                </div>
                 <div class="box">
                     <div class="right-street-col blue"></div>
                     <p class="right-street-name">MayFair</p>
@@ -131,7 +153,10 @@
                 <p class="bottom-street-name">Euston Road</p>
                 <p class="price price-bottom"> <span class="price-label">M</span> 100</p>
             </div>
-            <div class="box"></div>
+            <div class="box">
+                <p class="chance-text chance-text-bottom">Chance</p>
+                <font-awesome-icon icon="fa-solid fa-question" class=" question question-bottom"/>
+            </div>
             <div class="box">
                 <div class="bottom-street-col light-blue"></div>
                 <p class="bottom-street-name">The Angel, Islington</p>
@@ -142,13 +167,20 @@
                 <img class="station" src="../assets/img/train.png" alt="">
                 <p class="price price-bottom"> <span class="price-label">M</span> 200</p>
             </div>
-            <div class="box"></div>
+            <div class="box">
+                <p class="tax-text">Income Tax</p>
+                <p class="tax">*</p>
+                <p class="price tax-price">pay <span class="price-label">M</span> 200</p>
+            </div>
             <div class="box">
                 <div class="bottom-street-col brown"></div>
                 <p class="bottom-street-name">White Chapel Road</p>
                 <p class="price price-bottom"> <span class="price-label">M</span> 60</p>
             </div>
-            <div class="box"></div>
+            <div class="box">
+                <p class="chest-text chest-text-bottom">Community <br> Chest</p>
+                <img src="../assets/img/chest.png" alt="" class="chest chest-bottom">
+            </div>
             <div class="box">
                 <div class="bottom-street-col brown"></div>
                 <p class="bottom-street-name">Old Kent Road</p>
@@ -421,5 +453,189 @@ body{
     margin-top: 3px;
     margin-bottom: 1px;
 }
+
+/* Сектор шанс */
+
+.chance-text {
+    font-size: 10px;
+    position: absolute;
+}
+
+.chance-text-top {
+    transform: rotate(180deg);
+    bottom: -7px;
+    left: 15px;
+}
+
+.question {
+    font-size: 55px;
+}
+
+.question-top {
+    transform: rotate(180deg);
+    margin-top: 4px;
+    margin-left: 12px;
+    color: rgb(116, 219, 248);
+}
+
+.chance-text-right {
+    transform: rotate(270deg);
+    left: -7px;
+    top: 15px;
+}
+
+.question-right {
+    transform: rotate(270deg);
+    margin-top: 2px;
+    margin-left: 27px;
+    color: orangered;
+}
+
+.chance-text-bottom {
+    top: -7px;
+    left: 14px;
+}
+
+.question-bottom {
+    margin-top: 17px;
+    margin-left: 12px;
+    color: rgb(239, 6, 177);
+}
+
+/* Водопровод */
+
+.water-works-text {
+    font-size: 10px;
+    position: absolute;
+    transform: rotate(180deg);
+    bottom: -7px;
+    left: 3px;
+}
+
+.water-works {
+    font-size: 37px;
+    transform: rotate(180deg);
+    margin-top: 18px;
+    margin-left: 10px;
+}
+
+/* Электроподстанция */
+
+.electricity-text {
+    font-size: 10px;
+    transform: rotate(90deg);
+    position: absolute;
+    right: -10px;
+    top: 8px;
+    text-align: center;
+}
+
+.electricity {
+    display: block;
+    width: 35px;
+    height: 35px;
+    transform: rotate(90deg);
+    position: absolute;
+    right: 25px;
+    top: 11px;
+}
+
+/* Сундук */
+
+.chest-text {
+    font-size: 10px;
+    text-align: center;
+}
+
+.chest-text-left {
+    transform: rotate(90deg);
+    position: absolute;
+    right: -10px;
+    top: 7px;
+}
+
+.chest-text-right {
+    transform: rotate(270deg);
+    position: absolute;
+    left: -10px;
+    top: 7px;
+}
+
+.chest-text-bottom {
+    margin-top: 3px;
+    margin-bottom: 1px;
+}
+
+.chest {
+    display: block;
+    width: 59px;
+    height: 48px;
+    position: absolute;
+    
+}
+
+.chest-left {
+    transform: rotate(90deg);
+    top: 4px;
+    right: 17px;
+}
+
+.chest-right {
+    transform: rotate(270deg);
+    top: 7px;
+    right: -2px;
+}
+
+.chest-bottom {
+    bottom: 4px;
+}
+
+/* Налоги */
+
+.tax-text {
+    font-size: 10px;
+    text-align: center;
+    margin-top: 3px;
+    margin-bottom: 1px;
+}
+
+.tax {
+    font-size: 15px;
+    text-align: center;
+}
+
+.tax-price {
+    bottom: -12px;
+    left: 5px;
+}
+
+.supertax-text {
+    font-size: 10px;
+    text-align: center;
+    transform: rotate(270deg);
+    position: absolute;
+    left: -12px;
+    top: 12px;
+}
+
+.supertax {
+    display: block;
+    width: 37px;
+    height: 27px;
+    transform: rotate(270deg);
+    position: absolute;
+    left: 17px;
+    top: 15px;
+}
+
+.supertax-price {
+    font-size: 10px;
+    position: absolute;
+    transform: rotate(270deg);
+    top: 12px;
+    right: -14px;
+}
+
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faQuestion } from '@fortawesome/free-solid-svg-icons'
+import { faFaucetDrip } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUserSecret, faQuestion)
+library.add(faUserSecret, faQuestion, faFaucetDrip);
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
